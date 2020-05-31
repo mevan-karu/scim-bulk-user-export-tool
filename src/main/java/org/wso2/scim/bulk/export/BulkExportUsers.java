c194db6e6776949f6d4cd72616f325c1309f4c0e
mevan <mevan.15@cse.mrt.ac.lk>
2020-05-31 12:21:45 +0530

    Code improvements

--- src/main/java/org/wso2/scim/bulk/export/BulkExportUsers.java
+++ src/main/java/org/wso2/scim/bulk/export/BulkExportUsers.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.scim.bulk.export;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -37,7 +55,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main {
+public class BulkExportUsers {
 
     private static final String SCIM_USER_ENDPOINT = "scim2/Users";
     private static final String PATH_SEPARATOR = "/";
@@ -72,23 +90,17 @@ public class Main {
             attributesToExclude.addAll(Arrays.asList(args[5].split(",")));
         }
 
-        final SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial(null, (x509CertChain, authType) -> true)
-                .build();
-
-
         // Create a get request to retrieve list users from SCIM 2.0
         URIBuilder builder = new URIBuilder(hostAddress + PATH_SEPARATOR + SCIM_USER_ENDPOINT);
         if (attributes != null) {
             builder.setParameter("attributes", attributes);
         }
-        HttpGet request = new HttpGet(builder.build());
-        String auth = username + ":" + password;
-        byte[] encodedAuth = Base64.encodeBase64(
-                auth.getBytes(StandardCharsets.ISO_8859_1));
-        String authHeader = "Basic " + new String(encodedAuth);
-        // Set authorization headers.
-        request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
+
+        HttpGet request = HttpClient.createGetHttpRequest(builder, username, password);
+
+        final SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(null, (x509CertChain, authType) -> true)
+                .build();
 
         CloseableHttpClient client = HttpClientBuilder.create()
                 .setSSLContext(sslContext)
